ccdbaae279d2821ce2325f3f8898cc94ed314743
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2020-05-05 21:45:47 -0300

    project fn

---
+++ project_fn/learning/hello/func_v2.js
@@ -0,0 +1,11 @@
+const fdk=require('@fnproject/fdk');
+
+fdk.handle(function(input, ctx){
+  let name = 'World';
+  if (input.name) {
+    name = input.name;
+  }
+  console.log('\nInside Node Hello World function')
+  return {'message': 'Hello ' + name ,'ctx':ctx}
+})
+
