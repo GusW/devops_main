b538309c636cae1480912b09824d8d8197243cb2
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2020-08-07 11:55:55 -0300

    Terraforming OCI

--- oci/terraform/test01/datasources.tf
+++ oci/terraform/test01/datasources.tf
@@ -10,3 +10,9 @@ data "oci_objectstorage_namespace" "user_namespace" {
 resource "random_id" "tf_id" {
   byte_length = 2
 }
+
+data "oci_objectstorage_object" "read-hello-world-object" {
+  bucket = oci_objectstorage_object.hello-world-object-in-bucket.bucket
+  namespace = oci_objectstorage_object.hello-world-object-in-bucket.namespace
+  object = oci_objectstorage_object.hello-world-object-in-bucket.object
+}
--- oci/terraform/test01/storage.tf
+++ oci/terraform/test01/storage.tf
@@ -1,5 +1,4 @@
 resource "oci_objectstorage_bucket" "bucket_tf" {
-
   #Required
   compartment_id = var.compartment_ocid
   name           = "bucket-${random_id.tf_id.dec}"
@@ -7,3 +6,28 @@ resource "oci_objectstorage_bucket" "bucket_tf" {
   freeform_tags  = local.common_tags
   depends_on     = [oci_identity_policy.tf_allow_object_storage_lifecycle]
 }
+
+resource "oci_objectstorage_object" "hello-world-object-in-bucket" {
+    #Required
+    bucket = oci_objectstorage_bucket.bucket_tf.name
+    content = "Hello Galaxy"
+    namespace = data.oci_objectstorage_namespace.user_namespace.namespace
+    object = "my-new-object"
+    content_type = "text/text"
+}
+
+# report on the managed bucket resource by printing its OCID
+output "show-new-bucket" {
+  value = oci_objectstorage_bucket.bucket_tf.bucket_id
+}
+
+# report on the managed object resource by printing the full object
+output "show-new-object" {
+  value = oci_objectstorage_object.hello-world-object-in-bucket
+}
+
+# report on the managed object resource by printing the full object
+output "show-hello-world-object" {
+  value = data.oci_objectstorage_object.read-hello-world-object
+}
+
